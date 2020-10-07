aa78a681fb7b137c26c6bf91836d48e4fd4596a6
kiseok, Yang <rltjr219@gmail.com>
2020-10-07 23:35:31 +0900

    third commit

--- src/test/java/com/example/demo/test/Tests.java
+++ src/test/java/com/example/demo/test/Tests.java
@@ -1,13 +1,24 @@
 package com.example.demo.test;
 
+import com.example.demo.Demo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class Tests {
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @DisplayName("CI Test")
     @Test
@@ -16,4 +27,19 @@ public class Tests {
 
         assertThat(s).isEqualTo("Hello Java");
     }
+
+    @DisplayName("ObjectMapper Test")
+    @Test
+    void test_ob() throws JsonProcessingException {
+        Demo demo = new Demo();
+
+        demo.setJson("test json");
+
+        String om = objectMapper.writeValueAsString(demo);
+
+        System.out.println(om);
+
+        assertThat(om).isEqualTo("{\"json\":\"test json\"}");
+    }
+
 }
