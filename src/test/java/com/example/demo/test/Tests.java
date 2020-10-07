5679eaf35e9f6774ad11f615f1e66d507799eec5
kiseok, Yang <rltjr219@gmail.com>
2020-10-07 23:41:47 +0900

    fourth commit

--- src/test/java/com/example/demo/test/Tests.java
+++ src/test/java/com/example/demo/test/Tests.java
@@ -3,22 +3,45 @@ package com.example.demo.test;
 import com.example.demo.Demo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class Tests {
 
+    private MockMvc mockMvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
     @Autowired
     ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp()    {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilters(new CharacterEncodingFilter(StandardCharsets.UTF_8.name()))
+                .build()
+        ;
+    }
 
     @DisplayName("CI Test")
     @Test
@@ -40,6 +63,15 @@ public class Tests {
         System.out.println(om);
 
         assertThat(om).isEqualTo("{\"json\":\"test json\"}");
+    }
+
+    @DisplayName("Controller Test")
+    @Test
+    void controller_test()  throws Exception    {
+        mockMvc.perform(get("/api"))
+                .andDo(print())
+                .andExpect(status().isOk())
+        ;
     }
 
 }
